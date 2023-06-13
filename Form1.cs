4fcc50362ecf2dc44d2b2987d1b6b456111e2991
nikita.prigolovkin <vitalii.prigolovkin@gmail.com>
2023-06-13 17:49:07 +0300

    save-update 1.1

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -45,5 +46,38 @@ namespace Turtle_Ninja
 
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var bmp = new Bitmap(700, 400))
+            using (var gr = Graphics.FromImage(bmp))
+            {
+                gr.FillEllipse(Brushes.LimeGreen, 290, 85, 200, 280);
+                gr.FillEllipse(Brushes.Lime, 290, 100, 200, 250);
+                gr.FillEllipse(Brushes.Lime, 270, 260, 40, 40);
+                gr.FillEllipse(Brushes.Lime, 468, 260, 40, 40);
+                gr.FillEllipse(Brushes.Yellow, 335, 215, 110, 110);
+                gr.FillRectangle(Brushes.Yellow, 341, 155, 100, 130);
+                gr.FillRectangle(Brushes.Lime, 341, 334, 30, 50);
+                gr.FillRectangle(Brushes.Lime, 411, 334, 30, 50);
+                gr.FillRectangle(Brushes.Purple, 411, 360, 30, 15);
+                gr.FillRectangle(Brushes.Purple, 341, 360, 30, 15);
+                gr.FillEllipse(Brushes.Lime, 341, 60, 100, 80);
+                gr.FillEllipse(Brushes.Lime, 351, 40, 80, 70);
+                gr.FillRectangle(Brushes.Purple, 352, 50, 80, 20);
+                gr.FillEllipse(Brushes.Black, 365, 80, 15, 15);
+                gr.FillEllipse(Brushes.Black, 400, 80, 15, 15);
+                gr.FillEllipse(Brushes.White, 365, 80, 5, 5);
+                gr.FillEllipse(Brushes.White, 400, 80, 5, 5);
+                gr.FillRectangle(Brushes.Gray, 483, 230, 17, 100);
+                gr.FillRectangle(Brushes.SaddleBrown, 483, 100, 17, 130);
+                gr.FillRectangle(Brushes.SaddleBrown, 483, 290, 17, 130);
+                var path = System.IO.Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                    "Example.png");
+                bmp.Save(path);
+            }
+        }
+
     }
 }
